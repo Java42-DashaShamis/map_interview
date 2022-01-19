a052f60e1d2a53d33fb5434f543846c255d74ad6
vorabkin <vorabkin@gmail.com>
2022-01-19 09:53:19 +0200

    adds Vladimir's hw31 anagram review

--- src/telran/util/words/Anagram.java
+++ src/telran/util/words/Anagram.java
@@ -11,9 +11,19 @@ public class Anagram {
  * implementation requirements: O[N], using only one map and two passing's over (one is over word and other is over anagram)
  */
 	public static boolean isAnagram(String word, String anagram) {
+		/* V.R. This checking isn't enough. 
+		 *  What will be if word (or anagram) is empty or null?
+		 *  In both of cases exception will stop your application
+		 */
 		if(word.length()!=anagram.length()) {
 			return false;
 		}
+		/* V.R. Why TreeMap is selected? HashMap does the same (in our case) but
+		 * its complexity (O[1]) is better.
+		 * Anyway it is better to write
+		 * Map<Character, Integer> map = new HashMap<>();
+		 * 
+		 */
 		TreeMap<Character, Integer> map = new TreeMap<Character, Integer>();
 		for(int i = 0; i < word.length(); i++) {
 			Integer count = map.getOrDefault(word.toLowerCase().charAt(i), 0);
@@ -21,9 +31,19 @@ public class Anagram {
 		}
 		for(int i = 0; i < anagram.length(); i++) {
 			Integer count = map.getOrDefault(anagram.toLowerCase().charAt(i), 0);
+			/* V.R. It is the good place to check the count here
+			 *  if(count==0) return false;
+			 */
 			map.put(anagram.toLowerCase().charAt(i), count - 1);
 		}
+		/* V.R.
+		 * If to check count inside the previous loop then the following is redundant
+		 */
 		for(Integer i: map.values()) {
+			/* V.R. 
+			 *  i may be negative (in your implementation) and existing checking
+			 *  isn't enough
+			 */
 			if(i!=0) {
 				return false;
 			}
